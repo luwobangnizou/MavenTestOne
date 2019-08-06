5f2ec9df375341538263f8729baaff801757a401
luwobangnizou <906024671@qq.com>
2019-08-06 21:10:00 +0800

    temp

--- src/main/java/com/gray/CustomerServlet.java
+++ src/main/java/com/gray/CustomerServlet.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class CustomerServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //使用dopost方法
+        //使用dopost方法 temp
         doPost(req, resp);
     }
 
