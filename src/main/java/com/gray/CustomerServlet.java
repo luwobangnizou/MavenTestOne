c89fdbbe18f408d917481f246f219c9ae1ecb0ed
luwobangnizou <906024671@qq.com>
2019-08-06 18:31:58 +0800

    mavenTestOne项目提交到Git仓库中

---
+++ src/main/java/com/gray/CustomerServlet.java
@@ -0,0 +1,28 @@
+package com.gray;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Grayjeb
+ * @version 1.0
+ * @description PACKAGE_NAME
+ * @date 2019/4/24
+ */
+public class CustomerServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //使用dopost方法
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            request.setCharacterEncoding("UTF-8");
+        String username = request.getParameter("username");
+        System.out.println("username="+username);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yangluluhlims
+  Date: 2019/4/24
+  Time: 23:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="sa" method="post">
+    姓名:<input type="text" name="username"><br/>
+        <input type="submit" value="提交">
+</form>
+</body>
+</html>
